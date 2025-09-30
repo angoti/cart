7ea1be3736f8ef222b2f470edf9b1f36ffd49e22
angoti <angoti@gmail.com>
2025-09-30 16:48:34 -0300

    Remove leaked Google Firebase API key

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Configuração do Firebase - substitua pelos seus dados do projeto
+// Configuração do Firebase - usa variáveis de ambiente
 const firebaseConfig = {
-	//insira aqui os dados do seu projeto
+	apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Inicializar Firebase
